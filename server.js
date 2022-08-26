a4404005780da33c6d5979719548be76f42f553b
Benjamin Hites <bjhites@email.unc.edu>
2022-08-26 11:01:41 -0400

    text/html change

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	}
 	const server = http.createServer((req, res) => {
 		res.statusCode = 200;
-		res.setHeader('Content-Type', 'text/plain');
+		res.setHeader('Content-Type', 'text/html');
 		res.end(data);
 })
 
